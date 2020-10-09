08a1451581a4f5f90bb480ffae1ae96786b9f7a9
Soumya S <soumyas@thomsuninfocare.com>
2020-10-09 15:55:07 +0530

    Updated Currency dependant models
    Updated Currency dependant models - added Currency Denomination model

---
+++ Chef.Common.Models/Currency/CurrencyDenomination.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Chef.Common.Core;
+using Chef.Common.Types;
+
+namespace Chef.Common.Models
+{
+    public class CurrencyDenomination : Model
+    {
+        [Required]
+        [ForeignKey("Common.Currency")]
+        public int CurrencyId { get; set; }
+
+        public int Value { get; set; }
+
+        public DenominationType Type { get; set; }
+    }
+}
