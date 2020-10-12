097aa377bd7ed960b51416e7077b8b32c5257e09
Renju <renjug@thomsuninfocare.com>
2020-10-12 19:09:36 +0530

    New model created for financial year Period
    New model created for financial year Period

---
+++ Chef.Common.Models/FinancialYear/FinancialYearViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Chef.Common.Core;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Chef.Common.Models
+{
+  public class FinancialYearViewModel:Model
+    {
+       
+        public string Name { get; set; }
+        
+        public string Code { get; set; }       
+        public DateTime StartDate { get; set; }
+       
+        public DateTime EndDate { get; set; }
+
+        //TO DO require clarification
+        public string Frequency { get; set; }
+        public int FinancialYearId { get; set; }
+        public string Period { get; set; }
+        
+        public DateTime FromDate { get; set; }
+
+       
+        public DateTime ToDate { get; set; }
+        public string Status { get; set; }
+
+    }
+}
