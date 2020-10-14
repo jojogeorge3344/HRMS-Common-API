26dfd5f21586c63a706e7774bc3f7479be9880e1
karthick <karthick@thomsuninfocare.com>
2020-10-14 11:02:03 +0530

    Added identity Server
    Added identity Server

---
+++ Chef.Common.Samples/Chef.Common.MvcClient/Chef.Common.MvcClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chef.Common.MvcClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
