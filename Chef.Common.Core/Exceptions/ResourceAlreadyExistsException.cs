4f4ed6fecb916d7dde0c9c2627ccd1e12b997817
Reginold Bobin <reginoldb@thomsuninfocare.com>
2020-10-20 11:27:49 +0530

    Datatype fix

---
+++ Chef.Common.Core/Exceptions/ResourceAlreadyExistsException.cs
@@ -0,0 +1,27 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Runtime.Serialization;
+
+namespace Chef.Common.Exceptions
+{
+    [Serializable]
+    public class ResourceAlreadyExistsException<T> : Exception
+        where T : Model
+    {
+        public ResourceAlreadyExistsException()
+        {
+        }
+
+        public ResourceAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public ResourceAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ResourceAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
