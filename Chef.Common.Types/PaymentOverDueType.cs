68ffc270ff909d170b8858fd1175d89f1e6d0c93
Ramachandan <ramachandran@thomsuninfocare.com>
2020-11-13 15:09:01 +0530

    Payment over due type added

---
+++ Chef.Common.Types/PaymentOverDueType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Chef.Common.Types
+{
+    public enum PaymentOverDueType
+    {
+        [Description("Due Invoices Only")]
+        DueInvoicesOnly = 1,
+
+        [Description("Over Due Days")]
+        OverDueDays =2,
+
+        [Description("All Invoices")]
+        AllInvoices =3,
+    }
+}
