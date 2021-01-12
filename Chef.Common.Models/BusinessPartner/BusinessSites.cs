9f63e3132aad0827f7769507388c5069816296a9
Soumya S <soumyas@thomsuninfocare.com>
2021-01-12 16:45:45 +0530

    Added new model - BusinessSites
    Added new model - BusinessSites

---
+++ Chef.Common.Models/BusinessPartner/BusinessSites.cs
@@ -0,0 +1,21 @@
+﻿using Chef.Common.Core;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chef.Common.Models
+{
+    public class BusinessSites : Model
+    {
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Description { get; set; }
+
+        [Write(false)]
+        [Skip(true)]
+        public List<BusinessSitesDetail> BusinessSitesDetails { get; set; }
+    }
+}
