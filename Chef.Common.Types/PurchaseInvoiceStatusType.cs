237ec776215c45472a045be24674605d552d1d11
Renju <renjug@thomsuninfocare.com>
2021-03-01 14:29:25 +0530

    purchase invoice status type
    purchase invoice status type

---
+++ Chef.Common.Types/PurchaseInvoiceStatusType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Chef.Common.Types
+{
+    public enum PurchaseInvoiceStatusType
+    {
+        [Description("Open")]
+        Open = 1,
+
+        [Description("Match and Approved")]
+        MatchAndApproved = 2,       
+
+        [Description("On Hold")]
+        Hold = 3,
+        [Description("Approved")]
+        Approved = 4,
+    }
+}
