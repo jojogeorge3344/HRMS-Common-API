61e56ff121f8c2ed0c6fae68e37609c8468e189c
Renju <renjug@thomsuninfocare.com>
2021-07-27 13:16:37 +0530

    Model update
    Model update

---
+++ Chef.Common.Models/Node/SubModulePermissionView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class SubModulePermissionView
+    {
+        public int Id { get; set; }
+        public int PermissionId { get; set; }
+        public string PermissionName { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int ModuleId { get; set; }
+        public string ModuleName { get; set; }
+    }
+}
