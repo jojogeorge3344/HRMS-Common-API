02fb5a892b9a4212b3d7d6cb28e05ad855eefb1d
Ramachandan <ramachandran@thomsuninfocare.com>
2021-08-02 17:04:55 +0530

    Added item category enum

---
+++ Chef.Common.Types/ItemCategory.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Chef.Common.Types
+{
+    public enum ItemCategory
+    {
+        [Description("None")]
+        None = 0,
+
+        [Description("Inventory")]
+        Inventory = 1,
+
+        [Description("NonInventory")]
+        NonInventory = 2,
+
+        [Description("Service")]
+        Service = 3,
+
+        [Description("Asset")]
+        Asset = 4,
+
+        [Description("Tools")]
+        Tools = 5,
+
+        [Description("Expenses with Inventory")]
+        ExpenseswithInventory = 6
+
+        //[Description("SubContract")]
+        //SubContract = 7
+    }
+}
