685427b5a07f6c592f164ee58a418adf4f28eb3e
Ramachandan <ramachandran@thomsuninfocare.com>
2021-08-02 23:59:14 +0530

    added CostPrizeComponent model

---
+++ Chef.Common.Models/CostPrice/CostPrizeComponent.cs
@@ -0,0 +1,12 @@
+﻿using Chef.Common.Core;
+
+namespace Chef.Common.Models.Master
+{
+    public class CostPrizeComponent : Model
+    {
+        public int CpcId { get; set; }
+        public string CpcCode { get; set; }
+        public string CpcName { get; set; }
+
+    }
+}
