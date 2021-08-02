6f42ff8e772cc32a6bf648fe543ab47c9cd45287
anuvthomsun <anuv@thomsuninfocare.com>
2021-08-02 14:39:35 +0530

    changes in role

--- Chef.Common.Models/Role/RoleNodePermission.cs
+++ Chef.Common.Models/Role/RoleNodePermission.cs
@@ -10,8 +10,10 @@ namespace Chef.Common.Models
         public int RoleId { get; set; }
         [ForeignKey("Node")]
         public int NodeId { get; set; }
-        public string Name { get; set; }
+        public string NodeName { get; set; }
         public int ModuleId { get; set; }
+        [Write(false)]
+        [Skip(true)]
         public string ModuleName { get; set; }
 
         [ForeignKey("NodePermission")]
