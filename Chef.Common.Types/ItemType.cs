0e7551cb3cb9910cc212277c3aaf94ee8659cfa1
Ramachandan <ramachandran@thomsuninfocare.com>
2021-08-02 23:25:09 +0530

    added Item type enum

---
+++ Chef.Common.Types/ItemType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Chef.Common.Types
+{
+    public enum ItemType
+    {
+        None = 0,
+        [Description("Purchased")]
+        Purchased = 1,
+
+        [Description("Manufactured")]
+        Manufactured = 2,
+
+        [Description("Sub contracted")]
+        SubContracted = 3,
+
+        [Description("Others")]
+        Others = 4
+    }
+}
