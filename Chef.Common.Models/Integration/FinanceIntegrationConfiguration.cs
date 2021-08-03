2a3b02ea9cc4d473b2f9c1bacd745abe39bfae5c
Ramachandan <ramachandran@thomsuninfocare.com>
2021-08-03 08:00:30 +0530

    added integration models

---
+++ Chef.Common.Models/Integration/FinanceIntegrationConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+				public class FinanceIntegrationConfiguration : Model
+				{
+								public int ItemCategoryId { get; set; }
+
+								public string ItemCategoryName { get; set; }
+
+								public int ItemTypeId { get; set; }
+
+								public string ItemTypeName { get; set; }
+
+								public int CostPriceComponentId { get; set; }
+
+								public string CostPriceComponentName { get; set; }
+
+								public int ItemSegmentId { get; set; }
+
+								public string ItemSegmentName { get; set; }
+
+								public int ItemClassId { get; set; }
+
+								public string ItemClassName { get; set; }
+
+								public int ItemFamilyId { get; set; }
+
+								public string ItemFamilyName { get; set; }
+
+								public int ItemCommondityId { get; set; }
+
+								public string ItemCommondityName { get; set; }
+
+								public List<IntegrationControlAccount> IntegrationControlAccounts { get; set; }								 
+
+				}
+}
---
+++ Chef.Common.Models/Integration/IntegrationControlAccount.cs
@@ -0,0 +1,18 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+				public class IntegrationControlAccount : Model
+				{
+								public int ChartOfAccountId { get; set; }
+
+								public string ChartOfAccountCode { get; set; }
+
+								public string ChartOfAccountName { get; set; }
+				}
+} 
