d701927c6255f75260bffb961b11979163d142f4
Renju <renjug@thomsuninfocare.com>
2021-08-03 16:12:28 +0530

    Update submodulepermissionview model
    Update submodulepermissionview model

--- Chef.Common.Models/Node/SubModulePermissionView.cs
+++ Chef.Common.Models/Node/SubModulePermissionView.cs
@@ -10,6 +10,7 @@ namespace Chef.Common.Models
     {
         public int Id { get; set; }
         public int PermissionId { get; set; }
+        public int NodePermissionId { get; set; }
         public string PermissionName { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
