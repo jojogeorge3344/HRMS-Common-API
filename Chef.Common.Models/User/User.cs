4c22f8427ddaed067a51a53c89edc1d04374400e
anuvthomsun <anuv@thomsuninfocare.com>
2021-08-09 15:43:09 +0530

    change in user model

--- Chef.Common.Models/User/User.cs
+++ Chef.Common.Models/User/User.cs
@@ -5,6 +5,11 @@ namespace Chef.Common.Models
    public class User:Model
     {
         public string Code { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Emailid { get; set; }
+        public string TimeZone { get; set; }
+        public string Password { get; set; }
+        
     }
 }
