24b64c11de556908e318b5b7c35af4020f1edec3
anuvthomsun <anuv@thomsuninfocare.com>
2021-08-27 12:55:19 +0530

    change in model
    change in model

--- Chef.Common.Models/User/UserBranchView.cs
+++ Chef.Common.Models/User/UserBranchView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,17 @@ namespace Chef.Common.Models
         public string BranchCode { get; set; }
         public string BranchName { get; set; }
         public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string CityName { get; set; }
+        public string StateName { get; set; }
+        public string CountryName { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string CompanyCode { get; set; }
+        public string CompanyName { get; set; }
         public int IsBranchActive { get; set; }
+
     }
 }
