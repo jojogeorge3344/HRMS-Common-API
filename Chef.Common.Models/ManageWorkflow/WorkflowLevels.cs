757b2e61559c80e39e69512144d6138ef1c02490
Renju <renjug@thomsuninfocare.com>
2021-09-03 13:34:24 +0530

    Model update
    Model update

--- Chef.Common.Models/ManageWorkflow/WorkflowLevels.cs
+++ Chef.Common.Models/ManageWorkflow/WorkflowLevels.cs
@@ -24,6 +24,7 @@ namespace Chef.ApprovalSystem.Models
         public string Value { get; set; }
         public string datatype { get; set; }
 
+        public int level { get; set; }
         [Write(false)]
         [Skip(true)]
         public string LevelName { get; set; }
