94917938713b425275cef33cf4db8dc0f71384ca
Midhun Gopinath <midhung@thomsuninfocare.com>
2021-09-10 07:26:02 +0530

    adding user details modal

---
+++ Chef.Common.Models/User/UserMetaData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class UserMetaData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
