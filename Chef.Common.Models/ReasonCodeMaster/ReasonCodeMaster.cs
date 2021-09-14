13507459ca2236a089a9c5707025f772a35d54a3
athiraak <athirak@thomsuninfocare.com>
2021-09-14 14:14:27 +0530

    Create ReasonCodeMaster.cs

---
+++ Chef.Common.Models/ReasonCodeMaster/ReasonCodeMaster.cs
@@ -0,0 +1,14 @@
+﻿using Chef.Common.Core;
+using System.ComponentModel.DataAnnotations;
+using Chef.Trading.Types;
+
+
+namespace Chef.Trading.Models
+{
+    public class ReasonCodeMaster : Model
+    {
+        public string ReasonCode { get; set; }
+        public string Remarks { get; set; }
+        public bool isassigned { get; set; }
+    }
+}
