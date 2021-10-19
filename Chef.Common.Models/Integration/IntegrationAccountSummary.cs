f9f405a2ce0b18677102df33d6a7babf6b6caed5
Ramachandan <ramachandran@thomsuninfocare.com>
2021-10-19 22:06:12 +0530

    added finance integration models

---
+++ Chef.Common.Models/Integration/IntegrationAccountSummary.cs
@@ -0,0 +1,32 @@
+﻿using Chef.Common.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chef.Common.Models
+{
+    public class IntegrationAccountSummary : Model
+    {
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        public string AccountCode { get; set; }
+
+        [Required]
+        public string AccountDesc { get; set; }
+
+        [Required]
+        public decimal DebitValue { get; set; }
+
+        [Required]
+        public decimal DebitValueInBaseCurrency { get; set; }
+
+        [Required]
+        public decimal CreditValue { get; set; }
+
+        [Required]
+        public decimal CreditValueInBaseCurrency { get; set; }
+
+        [Required]
+        public bool IsDebit { get; set; }
+    }
+}
---
+++ Chef.Common.Models/Integration/IntegrationHeader.cs
@@ -0,0 +1,57 @@
+﻿using Chef.Common.Core;
+using Chef.Finance.Types;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chef.Common.Models
+{
+    public class IntegrationHeader : Model
+    {
+        public int BusinessPartnerId { get; set; }
+
+        public string BusinessPartnerCode { get; set; }
+
+        public string BusinessPartnerName { get; set; }
+
+        public DateTime DocumentDate { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; }
+
+        [StringLength(6)]
+        public string TransactionCurrencyCode { get; set; }
+
+        public int ExchangeRateId { get; set; }
+
+        public decimal ExchangeRate { get; set; }
+
+        public DateTime ExchangeDate { get; set; }
+
+        [StringLength(250)]
+        public string Narration { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal TotalAmountInBaseCurrency { get; set; }
+
+        [Required]
+        public int BranchId { get; set; }
+
+        public ApproveStatus ApproveStatus { get; set; }
+
+        public decimal TaxAmount { get; set; }
+
+        public DocumentType SourceDocumentType { get; set; }
+
+        public string SourceDocumentNumber { get; set; }
+
+        public int SourceDocumentId { get; set; }
+
+        public DateTime SourceDocumentDate { get; set; }
+
+        [Write(false)]
+        [Skip(true)]
+        public List<IntegrationAccountSummary> integrationAccountSummaries { get; set; }
+    }
+}
