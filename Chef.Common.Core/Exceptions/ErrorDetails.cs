a72845777571ce47664060c85ccfd6bc5524d70a
Midhun Gopinath <midhung@thomsuninfocare.com>
2021-11-29 15:45:30 +0530

    adding 2 modals for logging error to graylog

---
+++ Chef.Common.Core/Exceptions/ErrorDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Core
+{
+    public class ErrorDetails
+    {
+
+        public int StatusCode { get; set; }
+
+        public List<string> Messages { get; set; }
+
+        public List<ErrorExceptionMessage> ErrorMessages { get; set; }
+    }
+}
---
+++ Chef.Common.Core/Exceptions/ErrorExceptionMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Core
+{
+    public class ErrorExceptionMessage
+    {
+        public ErrorExceptionMessage(int code, string msg)
+        {
+            Message = msg;
+            Code = code;
+        }
+
+        public string Message { get; set; }
+
+
+        public int Code { get; set; }
+    }
+}
