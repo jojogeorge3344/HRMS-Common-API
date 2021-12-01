f3f0e49ff7807ec5431d4a2f2a08e55e1a3e45f3
Zeara <zearaas@thomsuninfocare.com>
2021-12-01 13:42:38 +0530

    Story #11608 : Added extra Fields in Employee

--- Chef.Common.Models/Employee.cs
+++ Chef.Common.Models/Employee.cs
@@ -26,5 +26,16 @@ namespace Chef.Common.Models
         public int CompanyId { get; set; }
 
         public string CompanyName { get; set; }
+
+        public DateTime DateOfJoining { get; set; }
+
+        public string Department { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Address { get; set; }
+
+        public bool IsActive { get; set; }
+
     }
 }
