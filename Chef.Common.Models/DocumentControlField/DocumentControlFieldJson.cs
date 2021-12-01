764974cd1aab039e069967321051ddc183b288b6
anuvthomsun <anuv@thomsuninfocare.com>
2021-12-01 10:09:49 +0530

    change in model
    change in model

---
+++ Chef.Common.Models/DocumentControlField/DocumentControlFieldJson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class DocumentControlFieldJson
+    {
+        public string JsonData { get; set; }
+        public string ColumnlabelJson { get; set; }
+        public int NodeID { get; set; }
+        public string DocumentName { get; set; }
+        public int DocumentId { get; set; }
+        public string DocumentNumber { get; set; }
+        public DateTime DocumentDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Reportfilename { get; set; }
+    }
+}
