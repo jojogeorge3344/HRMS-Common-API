6032918dd2ea8c98c677441462536f47995ff9f7
anuvthomsun <anuv@thomsuninfocare.com>
2021-12-03 16:17:15 +0530

    adding model for approve
    adding model for approve

---
+++ Chef.Common.Models/Node/ApproveRejectDocViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class ApproveRejectDocViewModel
+    {
+        public int DocumentID { get; set; }
+        public int Status { get; set; }
+        public string Remarks { get; set; }
+
+    }
+}
