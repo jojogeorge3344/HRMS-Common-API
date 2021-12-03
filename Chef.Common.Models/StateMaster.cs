f1ca2e66143ae51fb0c27feeae72410990706d54
Zeara <zearaas@thomsuninfocare.com>
2021-12-03 12:55:10 +0530

    Task #11640 : Added new master table for state

---
+++ Chef.Common.Models/StateMaster.cs
@@ -0,0 +1,22 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class StateMaster : Model
+    {
+        [Required]
+        [StringLength(126)]
+        public string Name { get; set; }
+
+        [Required]
+        [ForeignKey("Common.Country")]
+        public int CountryId { get; set; }
+    }
+}
