b9e6682ab8e3fb356c06479a0d35f0b58f230d92
Renju <renjug@thomsuninfocare.com>
2021-12-06 13:56:54 +0530

    Issue fix

---
+++ Chef.Common.Models/ManageWorkflow/NodeWorkflowSteps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class NodeWorkflowSteps
+    {
+        public int NodeDocumentId { get; set; }
+        public int WorkFlowId { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string WorkFlowName { get; set; }
+        public string NodeControlFieldName { get; set; }
+        public string ConditionName { get; set; }
+        public string Value { get; set; }
+        public string Datatype { get; set; }
+        public string Operators { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
---
+++ Chef.Common.Models/ManageWorkflow/WorkflowLevels.cs
@@ -0,0 +1,42 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class WorkflowLevels : Model
+    {
+        [ForeignKey("workflow")]
+        public int WorkflowId { get; set; }
+
+        public int NodeControlFieldId { get; set; }//Foreignkey from admin.nodecontrolfield
+
+        public string NodeControlFieldName { get; set; }
+
+        public string Condition { get; set; }
+
+        public string ConditionName { get; set; }
+
+        public string Value { get; set; }
+        public string DataType { get; set; }
+        [Write(false)]
+        [Skip(true)]
+        public int Level { get; set; }
+        [Write(false)]
+        [Skip(true)]
+        public string LevelName { get; set; }
+        [Write(false)]
+        [Skip(true)]
+        public int RoleID { get; set; }
+        [Write(false)]
+        [Skip(true)]
+        public string RoleName { get; set; }
+        [Write(false)]
+        [Skip(true)]
+        public bool IsDefault { get; set; }
+    }
+}
