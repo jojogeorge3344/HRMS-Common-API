b80e0d2b9da476fef276e3094af31ecf1b473b53
Arun Skaria <arun.sk@thomsuninfocare.com>
2022-01-17 11:24:54 +0530

    12962 Report Need to be saved in DMS for Approval Button

---
+++ Chef.Common.Models/Reports/FileSaving.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    [Serializable]
+    public class FileSaving
+    {
+        /// <summary>
+        /// Saving the byte array data from report viewer
+        /// </summary>
+        public byte[] ByteData { get; set; }
+
+        /// <summary>
+        /// Unique name which can be given to the report name while saving pdf
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Id from database
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Module name or name of the report. Folders are created according to this name
+        /// </summary>
+        public string Module { get; set; }
+
+        /// <summary>
+        /// true: when the file is given for approval, while viewing it is showed in preview
+        /// false: supporting documents. viewing is showed as link
+        /// </summary>
+        public bool IsPrintPreview { get; set; } = false;
+
+    }
+}
