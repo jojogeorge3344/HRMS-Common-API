2844b049a7ef7abade3cd48b3f8fc03377bfaee7
Ramachandan <ramachandran@thomsuninfocare.com>
2022-02-08 05:03:16 +0530

    added new model PurchaseInvoiceLineItem

---
+++ Chef.Common.Models/Invoices/PurchaseInvoiceLineItem.cs
@@ -0,0 +1,31 @@
+﻿using Chef.Common.Core;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Chef.Finance.Models
+{
+    public class PurchaseInvoiceLineItem :TransactionModel
+    {
+        [Required]
+        [ForeignKey("PurchaseInvoice")]
+        public int PurchaseInvoiceId { get; set; }
+
+        [Required]
+        public int LineNumber { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public decimal TaxPercentage { get; set; }
+
+        [Required]
+        public decimal TaxAmount { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+    }
+}
