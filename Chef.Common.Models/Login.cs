f291eb3b1a2ac8951118c496b6bc1f2b5f3eafb5
Antony  John <antonyj@thomsuninfocare.com>
2022-06-13 10:58:19 +0530

    Model For Login

---
+++ Chef.Common.Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+   public class Login :Model
+    {
+        [EmailAddress]
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
