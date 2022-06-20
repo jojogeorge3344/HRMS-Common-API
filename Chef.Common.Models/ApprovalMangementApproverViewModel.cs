c390cdb3cba0de6db77851212db348bfb1c33d6b
Antony  John <antonyj@thomsuninfocare.com>
2022-06-20 14:29:16 +0530

    ApprovalMangement

---
+++ Chef.Common.Models/ApprovalMangementApproverViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+   public  class ApprovalMangementApproverViewModel
+    {
+        public int Level { get; set; }
+
+        public string Name { get; set; }
+
+        public string remarks { get; set; }
+
+        public int Status { get; set; }
+    }
+}
