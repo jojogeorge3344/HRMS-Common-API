50b014694051c5734389dcc497bd174f34845851
Antony  John <antonyj@thomsuninfocare.com>
2022-06-29 16:42:30 +0530

    Signature Model

---
+++ Chef.Common.Models/UserSignature.cs
@@ -0,0 +1,22 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class UserSignature:Model
+    {
+        [Required]
+        [ForeignKey("Common.User")]
+        public int UserId { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public byte[] Signature { get; set; }
+    }
+}
