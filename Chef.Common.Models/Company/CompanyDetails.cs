29271b7c120e535a7cdf52291883dba8443023c2
anuvthomsun <anuv@thomsuninfocare.com>
2022-07-22 15:25:01 +0530

    adding company details model
    adding company details model

---
+++ Chef.Common.Models/Company/CompanyDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class CompanyDetails
+    {
+        public string CompanyDetail { get; set; }
+        public string CompanyName { get; set; }
+        public string TRNNo { get; set; }
+    }
+}
