b1fe095f8c692b73eb16236e11784cfadc3079a0
athiraak <athirak@thomsuninfocare.com>
2022-09-03 00:28:24 +0530

    Task #18016-add designation and department
    Need to console add designation and department

---
+++ Chef.Common.Models/Department/Department.cs
@@ -0,0 +1,21 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class Department : Model
+     {
+
+            public string Code { get; set; }
+            public string Name { get; set; }
+            
+
+
+
+     }
+    
+}
---
+++ Chef.Common.Models/Designation/Designation.cs
@@ -0,0 +1,19 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class Designation: Model
+    {
+        
+            public string Code { get; set; }
+            public string Name { get; set; }
+
+            
+        
+    }
+}
