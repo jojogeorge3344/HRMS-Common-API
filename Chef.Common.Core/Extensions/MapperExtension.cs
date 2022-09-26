ce3276a6365c286106428055ffb83b0946eceac2
karthick <karthick@thomsuninfocare.com>
2022-09-26 13:42:54 +0530

    downgrade automapper packages
    downgrade automapper packages

--- Chef.Common.Core/Extensions/MapperExtension.cs
+++ Chef.Common.Core/Extensions/MapperExtension.cs
@@ -6,13 +6,7 @@ namespace Chef.Common.Core
     {
         public static IMappingExpression<TSource, TDest> IgnoreAllUnmapped<TSource, TDest>(this IMappingExpression<TSource, TDest> expression)
         {
-            System.Type destinationType = typeof(TDest);
-
-            foreach (System.Reflection.PropertyInfo property in destinationType.GetProperties())
-            {
-                expression.ForMember(property.Name, opt => opt.Ignore());
-            }
-
+            expression.ForAllOtherMembers(opt => opt.Ignore());
             return expression;
         }
     }
