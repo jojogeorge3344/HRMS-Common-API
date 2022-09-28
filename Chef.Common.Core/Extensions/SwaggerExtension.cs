decf17e6e05bc4c9bc2b10dcb6a14dc2e9c4dc4b
karthick <karthick@thomsuninfocare.com>
2022-09-28 19:45:44 +0530

    Swagger update
    Swagger update

---
+++ Chef.Common.Core/Extensions/SwaggerExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chef.Common.Core.Extensions
+{
+	public static class SwaggerExtension
+	{
+        public static void ConfigureSwagger(
+            this IServiceCollection services,
+            string version,
+            string title,
+            string description,
+            string terms)
+        {
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "ToDo API";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                };
+            });
+        }
+    }
+}
+
