ba347288d4a8b23e01ea61e90e3e37a007151d07
karthick <karthick@thomsuninfocare.com>
2022-10-01 11:02:15 +0530

    Moved Base Service to Core
    Moved Base Service to Core

---
+++ Chef.Common.Core/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Chef.Common.Repositories;
+
+namespace Chef.Common.Services
+{
+    public abstract class BaseService : IBaseService
+    {
+        public IServiceSession ServiceSession { get; set; }
+
+        public IUnitOfWorkSession UnitOfWorkSession(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+        { 
+            return ServiceSession.UnitOfWorkSession(isolationLevel);
+        }
+    }
+}
