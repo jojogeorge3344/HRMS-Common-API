7ddcab7e042e7b81fdcf8f141d7b36513c247365
karthick <karthick@thomsuninfocare.com>
2022-10-01 10:49:08 +0530

    Moved Generic Service to Common
    Moved Generic Service to Common

---
+++ Chef.Common.Core/Exceptions/CustomExceptions/BankCannotBeDeletedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Chef.Common.Exceptions
+{
+    [Serializable]
+    public class BankCannotBeDeletedException : Exception
+    {
+        public BankCannotBeDeletedException()
+        {
+        }
+
+        public BankCannotBeDeletedException(string message) : base(message)
+        {
+        }
+
+        public BankCannotBeDeletedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BankCannotBeDeletedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
