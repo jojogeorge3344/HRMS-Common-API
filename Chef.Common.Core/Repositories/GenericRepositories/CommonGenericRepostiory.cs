a1d0509b9533f61f8d37aa7e7a4f67b509a234ff
karthick <karthick@thomsuninfocare.com>
2022-10-01 15:19:13 +0530

    Move CommonGeneric Repo to console
    Move CommonGeneric Repo to console

---
+++ Chef.Common.Core/Repositories/GenericRepositories/CommonGenericRepostiory.cs
@@ -0,0 +1,33 @@
+﻿using Chef.Common.Core;
+using Chef.Common.Repositories;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Repositories
+{
+    public abstract class CommonGenericRepository<TModel> : BaseRepository, ICommonGenericRepository<TModel>
+        where TModel : Model
+    {
+        public virtual async Task<int> DeleteAsync(int id)
+          => await GenericRepository<TModel>().DeleteAsync(id);
+
+        public async Task<IEnumerable<TModel>> GetAllAsync(CancellationToken cancellationToken = default)
+        => await GenericRepository<TModel>().GetAllAsync(cancellationToken);
+
+        public virtual async Task<TModel> GetAsync(int id, CancellationToken cancellationToken = default)
+        => await GenericRepository<TModel>().GetAsync(id, cancellationToken);
+
+        public virtual async Task<IEnumerable<TModel>> GetRecordsAsync(int noOfRecords, CancellationToken cancellationToken = default)
+        => await GenericRepository<TModel>().GetRecordsAsync(noOfRecords, cancellationToken);
+
+        public virtual async Task<IEnumerable<TModel>> GetRecordsAsync(SqlSearch sqlSearch = null, CancellationToken cancellationToken = default)
+        => await GenericRepository<TModel>().GetRecordsAsync(sqlSearch, cancellationToken);
+
+        public virtual async Task<int> InsertAsync(TModel obj)
+        => await GenericRepository<TModel>().InsertAsync(obj);
+
+        public virtual async Task<int> UpdateAsync(TModel obj)
+        => await GenericRepository<TModel>().UpdateAsync(obj);
+    }
+}
---
+++ Chef.Common.Core/Repositories/GenericRepositories/Interfaces/ICommonGenericRepostiory.cs
@@ -0,0 +1,18 @@
+﻿using Chef.Common.Repositories;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Repositories
+{
+    public interface ICommonGenericRepository<TModel>
+    {
+        Task<TModel> GetAsync(int id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<TModel>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<TModel>> GetRecordsAsync(SqlSearch sqlSearch = null, CancellationToken cancellationToken = default);
+        Task<IEnumerable<TModel>> GetRecordsAsync(int noOfRecords, CancellationToken cancellationToken = default);
+        Task<int> InsertAsync(TModel obj);
+        Task<int> UpdateAsync(TModel obj);
+        Task<int> DeleteAsync(int id);
+    }
+}
