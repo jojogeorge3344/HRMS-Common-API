1c741912129340f6d69c0997b405e1ceeeade84d
karthick <karthick@thomsuninfocare.com>
2022-10-02 22:45:43 +0530

    Logging interceptor
    Logging interceptor

--- Chef.Common.Core/Logging/LoggingInterceptor.cs
+++ Chef.Common.Core/Logging/LoggingInterceptor.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Linq;
 using Castle.DynamicProxy;
+using Microsoft.Extensions.Logging;
 
 namespace Chef.Common.Core.Logging
 {
     public class LoggingInterceptor : IInterceptor
     {
+        private readonly ILogger<LoggingInterceptor> logger;
+
+        public LoggingInterceptor(ILogger<LoggingInterceptor> logger)
+        {
+            this.logger = logger;
+        }
+
         public void Intercept(IInvocation invocation)
         {
-            Console.WriteLine($">> {invocation.Method.Name}");
+            //TODO optimize for the production.
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            logger.LogInformation($">> {invocation.TargetType.FullName}.{invocation.Method.Name}");
 
             // Invoke the method
             invocation.Proceed();
 
-            Console.WriteLine($"<< {invocation.Method.Name}");
+            watch.Stop();
+
+            logger.LogInformation($"<< {invocation.TargetType.FullName}.{invocation.Method.Name} :: {watch.ElapsedMilliseconds} ms");
         }
     }
 }
