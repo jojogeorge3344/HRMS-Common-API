3ddd49c2895c443ce2af0f9fcde3fc16cbd42b77
karthick <karthick@thomsuninfocare.com>
2022-11-10 18:24:40 +0530

    authentication
    authentication

--- Chef.Common.Authentication/Models/MappingProfile.cs
+++ Chef.Common.Authentication/Models/MappingProfile.cs
@@ -7,7 +7,9 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<ApplicationUser, UserDto>();
+        CreateMap<ApplicationUser, UserDto>()
+            .ForMember(dest => dest.UserId, Opt => Opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.BranchId, opt => opt.MapFrom(src => src.DefaultBranchId));
 
         //Email and username will be same.
         CreateMap<RegisterDto, ApplicationUser>()
--- Chef.Common.Authentication/Models/UserDto.cs
+++ Chef.Common.Authentication/Models/UserDto.cs
@@ -1,6 +1,10 @@
 ﻿public class UserDto
 {
+    public string? UserId { get; set; }
     public string? UserName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public string? TimeZone { get; set; }
-    public int DefaultBranchId { get; set; }
+    public string? Email { get; set; }
+    public int BranchId { get; set; }
 }
