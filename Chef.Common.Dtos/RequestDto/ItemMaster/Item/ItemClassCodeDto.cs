5b17ada5b9ff8e4ef32eae4544016e844947ad23
Justin V J <justinvj@thomsuninfocare.com>
2022-11-18 19:35:45 +0530

    Employee import

---
+++ Chef.Common.Dtos/RequestDto/ItemMaster/Item/ItemClassCodeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Dtos  
+{
+    public class ItemClassCodeDto
+    {
+        public int Id { get; set; }
+        public int ItemFamilyId { get; set; }
+        public string ItemFamilyCode { get; set; }
+        public string ItemClassCode { get; set; }
+        public string ItemClassName { get; set; }
+    }
+}
---
+++ Chef.Common.Dtos/RequestDto/ItemMaster/Item/ItemMasterCodeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Dtos
+{
+    public class ItemMasterCodeDto
+    {
+        public int Id { get; set; }
+        public int ItemCommodityId { get; set; }
+        public string ItemCommodityCode { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+    }
+}
