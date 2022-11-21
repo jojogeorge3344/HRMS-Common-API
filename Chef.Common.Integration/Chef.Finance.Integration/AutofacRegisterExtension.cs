c16f5369e174a631c96dd157b42f8ca23a771de6
karthick <karthick@thomsuninfocare.com>
2022-11-21 14:55:00 +0530

    Removed unused projects
    Removed unused projects

---
+++ Chef.Common.Integration/Chef.Finance.Integration/AutofacRegisterExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Autofac;
+
+namespace Chef.Finance.Integration;
+
+public static class AutofacRegisterExtension
+{
+    public static void RegisterFinanceModule(this ContainerBuilder builder)
+    {
+    }
+}
+
