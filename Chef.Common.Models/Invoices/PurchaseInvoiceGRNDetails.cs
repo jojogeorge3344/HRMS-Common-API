36c96815a2a76337a6d8a9a901e878d46f38d488
Mohammed Salih <mohammed.s@thomsuninfocare.com>
2023-01-06 18:34:49 +0530

    Task #21212: match and approval grn insert changes

---
+++ Chef.Common.Models/Invoices/PurchaseInvoiceGRNDetails.cs
@@ -0,0 +1,22 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models
+{
+    public class PurchaseInvoiceGRNDetails : Model
+    {
+        public int PoId { get; set; }
+        public string GRNNumber { get; set; }
+        public DateTime? GRNDate { get; set; }
+        public decimal GRNAmount { get; set; }
+        public decimal GRNTaxAmount { get; set; }
+        public decimal GRNTotalAmount { get; set; }
+        public decimal GRNTotalAmountInBaseCurrency { get; set; }
+        public decimal GRNExchangeRate { get; set; }
+        public DateTime? GRNExchangeDate { get; set; }
+    }
+}
