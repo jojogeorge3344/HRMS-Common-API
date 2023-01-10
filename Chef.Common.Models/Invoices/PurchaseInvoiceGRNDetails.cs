570f76213a47da97899d7260caeadcb8f87f64fa
Mohammed Salih <mohammed.s@thomsuninfocare.com>
2023-01-10 13:45:43 +0530

    Task #21212: PurchaseInvoiceGRNDetails model changes

--- Chef.Common.Models/Invoices/PurchaseInvoiceGRNDetails.cs
+++ Chef.Common.Models/Invoices/PurchaseInvoiceGRNDetails.cs
@@ -1,22 +1,17 @@
 ﻿using Chef.Common.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Chef.Common.Models
+namespace Chef.Finance.Models;
+
+public class PurchaseInvoiceGRNDetails : Model
 {
-    public class PurchaseInvoiceGRNDetails : Model
-    {
-        public int PoId { get; set; }
-        public string GRNNumber { get; set; }
-        public DateTime? GRNDate { get; set; }
-        public decimal GRNAmount { get; set; }
-        public decimal GRNTaxAmount { get; set; }
-        public decimal GRNTotalAmount { get; set; }
-        public decimal GRNTotalAmountInBaseCurrency { get; set; }
-        public decimal GRNExchangeRate { get; set; }
-        public DateTime? GRNExchangeDate { get; set; }
-    }
+    public int PurchaseInvoiceId { get; set; }
+    public string GRNNumber { get; set; }
+    public DateTime? GRNDate { get; set; }
+    public decimal GRNAmount { get; set; }
+    public decimal GRNTaxAmount { get; set; }
+    public decimal GRNTotalAmount { get; set; }
+    public decimal GRNTotalAmountInBaseCurrency { get; set; }
+    public decimal GRNExchangeRate { get; set; }
+    public DateTime? GRNExchangeDate { get; set; }
 }
