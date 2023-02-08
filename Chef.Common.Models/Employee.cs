86ec53001bbf847fb4781127fcb09b364ee1acc1
Mohammed Salih <mohammed.s@thomsuninfocare.com>
2023-02-08 13:53:41 +0530

    Bug #22110: business partner import validation added

--- Chef.Common.Models/Employee.cs
+++ Chef.Common.Models/Employee.cs
@@ -8,26 +8,26 @@ namespace Chef.Common.Models
     public class Employee : Model
     {
         [Required]
-        [MaxLength(10, ErrorMessage = "Employee code should be maximum length of 10 characters")]
+        [StringLength(10, ErrorMessage = "Employee code should be maximum length of 10 characters")]
         [RegularExpression("^(?!0*$)[A-Za-z0-9\\-\\/]+$", ErrorMessage = "Employee code should be alphanumeric only")]
         public string EmployeeCode { get; set; }
 
         [Required]
-        [MaxLength(50, ErrorMessage = "First name should be maximum length of 50 characters")]
+        [StringLength(50, ErrorMessage = "First name should be maximum length of 50 characters")]
         [RegularExpression("[a-zA-Z ]*", ErrorMessage = "First name should be alphabet only")]
         public string FirstName { get; set; }
 
-        [MaxLength(50, ErrorMessage = "Middle name should be maximum length of 50 characters")]
+        [StringLength(50, ErrorMessage = "Middle name should be maximum length of 50 characters")]
         [RegularExpression("[a-zA-Z ]*", ErrorMessage = "Middle name should be alphabet only")]
         public string MiddleName { get; set; }
 
         [Required]
-        [MaxLength(50, ErrorMessage = "Last name should be maximum length of 50 characters")]
+        [StringLength(50, ErrorMessage = "Last name should be maximum length of 50 characters")]
         [RegularExpression("[a-zA-Z ]*", ErrorMessage = "Last name should be alphabet only")]
         public string LastName { get; set; }
 
         [Required]
-        [MaxLength(50, ErrorMessage = "Display name should be maximum length of 50 characters")]
+        [StringLength(50, ErrorMessage = "Display name should be maximum length of 50 characters")]
         [RegularExpression("[a-zA-Z ]*", ErrorMessage = "Display name should be alphabet only")]
         public string DisplayName { get; set; }
 
@@ -42,8 +42,7 @@ namespace Chef.Common.Models
         public string Email { get; set; }
 
         [Required]
-        [MinLength(10, ErrorMessage = "Phone number should be minimum length of 10 characters")]
-        [MaxLength(18, ErrorMessage = "Phone number should be maximum length of 18 characters")]
+        [StringLength(maximumLength: 18, MinimumLength = 10, ErrorMessage = "Phone number should be minimum of 10 and maximum of 18 characters")]
         [RegularExpression("^([+]?[0-9- ]{1,18})+$", ErrorMessage = "Invalid phone number")]
         public string Phone { get; set; }
 
@@ -66,7 +65,7 @@ namespace Chef.Common.Models
         public int DesignationId { get; set; }
 
         [Required]
-        [MaxLength(100, ErrorMessage = "Address should be maximum length of 100 characters")]
+        [StringLength(100, ErrorMessage = "Address should be maximum length of 100 characters")]
         public string Address { get; set; }
 
         public bool IsActive { get; set; } = true;
