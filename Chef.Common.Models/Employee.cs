62fbf3a2b0557c2c8a8ea28b3224c36e8552ac84
Mohammed Salih <mohammed.s@thomsuninfocare.com>
2023-02-08 12:03:15 +0530

    Bug #22110: employee import validation added

--- Chef.Common.Models/Employee.cs
+++ Chef.Common.Models/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using Chef.Common.Core;
 using Chef.Common.Types;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Chef.Common.Models
 {
@@ -8,36 +9,61 @@ namespace Chef.Common.Models
     {
         public string EmployeeCode { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "First name should be maximum length of 50 characters")]
+        [RegularExpression("[a-zA-Z ]*", ErrorMessage = "First name should be alphabet only")]
         public string FirstName { get; set; }
 
+        [MaxLength(50, ErrorMessage = "Middle name should be maximum length of 50 characters")]
+        [RegularExpression("[a-zA-Z ]*", ErrorMessage = "Middle name should be alphabet only")]
         public string MiddleName { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "Last name should be maximum length of 50 characters")]
+        [RegularExpression("[a-zA-Z ]*", ErrorMessage = "Last name should be alphabet only")]
         public string LastName { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "Display name should be maximum length of 50 characters")]
+        [RegularExpression("[a-zA-Z ]*", ErrorMessage = "Display name should be alphabet only")]
         public string DisplayName { get; set; }
 
+        [Required]
         public GenderType Gender { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
 
+        [Required]
+        [RegularExpression("^\\S+@\\S+\\.\\S+$", ErrorMessage = "Invalid email")]
         public string Email { get; set; }
 
+        [Required]
+        [MinLength(10, ErrorMessage = "Phone number should be minimum length of 10 characters")]
+        [MaxLength(18, ErrorMessage = "Phone number should be maximum length of 18 characters")]
+        [RegularExpression("^([+]?[0-9- ]{1,18})+$", ErrorMessage = "Invalid phone number")]
         public string Phone { get; set; }
 
         public int CompanyId { get; set; }
 
+        [Required]
         public string CompanyName { get; set; }
 
+        [Required]
         public DateTime DateOfJoining { get; set; }
 
+        [Required]
         public string Department { get; set; }
 
         public int DepartmentId { get; set; }
 
+        [Required]
         public string Designation { get; set; }
 
         public int DesignationId { get; set; }
 
+        [Required]
+        [MaxLength(100, ErrorMessage = "Address should be maximum length of 100 characters")]
         public string Address { get; set; }
 
         public bool IsActive { get; set; } = true;
