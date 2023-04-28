d892a0c0e9db48fc982c1ee63af4eb7cdc8d8561
Antony John <antony.john@swiipr.com>
2023-04-28 14:48:01 +0530

    Model for Attachment

---
+++ Chef.Common.Models/BusinessPartner/BusinessPartnerDocumentAttachemnt.cs
@@ -0,0 +1,17 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models;
+
+public class BusinessPartnerDocumentAttachemnt:Model
+{
+    public int BusinessPartnerDocumentId { get; set; }
+
+    public string FileName { get; set; }
+
+    public int FileId { get; set; }
+}
---
+++ Chef.Common.Models/BusinessPartner/BusinessPartnerDocuments.cs
@@ -0,0 +1,23 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Common.Models;
+
+public class BusinessPartnerDocuments : Model
+{
+    public string DocumentName { get; set; }
+    public int DocumentTypeId { get; set; }
+
+    public int BusinessPartnerId { get; set; }
+
+    public DateTime ExpireDate { get; set; }
+
+    public DateTime IssueDate { get; set; }
+
+    public bool IsAttachment { get; set; }
+
+}
