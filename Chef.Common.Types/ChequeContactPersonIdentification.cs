13af36c79c50d1fe59b428938889796be8ec3837
Mohammed Salih <mohammed.s@thomsuninfocare.com>
2023-05-10 10:03:56 +0530

    Task #26170: ChequeDeliveredDetail Get APIs

--- Chef.Common.Types/ChequeContactPersonIdentification.cs
+++ Chef.Common.Types/ChequeContactPersonIdentification.cs
@@ -1,8 +1,13 @@
-﻿namespace Chef.Common.Types;
+﻿using System.ComponentModel;
+
+namespace Chef.Common.Types;
 
 public enum ChequeContactPersonIdentification
 {
+    [Description("Emirates ID")]
     EmiratesID = 1,
     Passport,
+
+    [Description("AW Bill")]
     AWBill
 }
--- Chef.Common.Types/ChequeDeliveryType.cs
+++ Chef.Common.Types/ChequeDeliveryType.cs
@@ -1,15 +1,8 @@
-﻿using System.ComponentModel;
-
-namespace Chef.Common.Types;
+﻿namespace Chef.Common.Types;
 
 public enum ChequeDeliveryType
 {
-    [Description("Courier")]
     Courier = 1,
-
-    [Description("Person")]
     Person,
-
-    [Description("Delivery")]
     Delivery
 }
