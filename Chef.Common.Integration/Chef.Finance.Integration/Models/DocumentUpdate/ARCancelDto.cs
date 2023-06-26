1d8dd06fc0f0050a54df2f93c320d2660518d10d
Antony John <antony.john@swiipr.com>
2023-06-26 11:01:34 +0530

    Invoice and Return Cancel in AR

---
+++ Chef.Common.Integration/Chef.Finance.Integration/Models/DocumentUpdate/ARCancelDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.Finance.Integration.Models;
+
+public class ARCancelDto
+{
+    public string DocumentNo { get; set; }
+    public string TransOrigin { get; set; }
+    public string TransType { get; set; }
+    public bool Status { get; set; }
+}
